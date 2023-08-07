e029abc444c408d09ad6c5f3e97e83760ba5be6c
Ayman BEN AMOR <ayman92.benamor@gmail.com>
2023-08-07 18:21:37 +0200

    initial commit

---
+++ src/test/java/org/demo/springelasticsearch/SpringElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo.springelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringElasticsearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
